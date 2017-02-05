7d58f1b13a1914e67b22045aa3cab9c3cee434f3
yfsoftcom <yfsoftcom@126.com>
2017-02-05 11:20:45 +0800

    add arg

--- yf-code-pull-hook.sh
+++ yf-code-pull-hook.sh
@@ -5,32 +5,37 @@
 clear
 
 # TODO: change the dir
-PROJECT_DIR="/home/yf/nodejs/projects/"
-WEB_SITE_DIR=$PROJECT_DIR"demo.yunplus.io/"
+PROJECT_DIR="/home/yf/nodejs/projects"
+WEB_SITE_DIR="$PROJECT_DIR/demo.yunplus.io"
 
 
 #没有值
 if [ -z $1 ]; then
   echo ">>>>>>>> Welcome there"
-  echo ">>>>>>>> Shell Require A Argument; Such As : list , pull ..."
+  echo ">>>>>>>> Shell Require 2 Arguments; Such As : list -w/-p , pull -w/-p ..."
   exit
 
 elif [ $1 = "list" ]; then
   echo ">>>>>>>> Show All The Projects"
-  ls -p $WEB_SITE_DIR | grep /
-  exit
-
-elif [ $1 = "add" ]; then
-  echo ">>>>>>>> Add A Project"
-
+  if [ $2 = "-p" ]; then
+    ls -p $PROJECT_DIR/ | grep /
+  elif [ $2 = "-w" ]; then
+    ls -p $WEB_SITE_DIR/ | grep /
+  fi
   exit
 
 elif [ $1 = "pull" ]; then
   echo ">>>>>>>> Pull The Projects Code"
+  if [ $2 = "-p" ]; then
+    cd $PROJECT_DIR/$3
+    git pull
+    pm2 restart $3
+  elif [ $2 = "-w" ]; then
+    cd $WEB_SITE_DIR/$3
+    git pull
+  fi
 
-  cd $WEB_SITE_DIR$2
 
-  git pull
 
   exit
 fi
